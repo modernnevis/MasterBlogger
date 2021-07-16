cdb3c815a7972e4977c844d5fe90ae83fd8ed940
yaser shakoori <yasershakoori@outlook.com>
2021-07-16 12:12:38 +0430

    Comment Layer Implemented - Application Layer Structure Created

---
+++ MB.Domain/CommentAgg/Statuses.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MB.Domain.CommentAgg
+{
+    public static class Statuses
+    {
+        public static int New = 0;
+        public static int Confirmed = 1;
+        public static int Canceled = 2;
+
+    }
+}
